3ac8e3571a5f14d5e30452a0110c5646c4d46109
wsxzwps <wsxzwps43@126.com>
2019-03-09 05:41:45 +0800

    teacher forcing

--- model.py
+++ model.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
+import random
+
 class EncoderRNN(nn.Module):
     def __init__(self, vocab_size, embedding_size, hidden_size, batch_size, embedding=None):
         super(EncoderRNN, self).__init__()
@@ -26,6 +28,8 @@ class DecoderRNN(nn.Module):
         super(DecoderRNN, self).__init__()
         self.hidden_size = hidden_size
         self.batch_size = batch_size
+        self.embedding_size = embedding_size
+        self.sos_id = 2
 
         self.embedding = nn.Embedding(vocab_size, embedding_size)
         if embedding is not None:
@@ -34,9 +38,21 @@ class DecoderRNN(nn.Module):
         self.out = nn.Linear(hidden_size, vocab_size)
 
 
-    def forward(self, input, hidden):
-        embs = self.embedding(input)
-        output, hidden = self.gru(embs, hidden)
-        output = F.log_softmax(self.out(output), dim=1)
+    def forward(self, input, hidden, maxlen, teacher_forcing_ratio=1):
+        if random.random() < teacher_forcing_ratio
+            embs = self.embedding(input)
+            output, hidden = self.gru(embs, hidden)
+            output = F.log_softmax(self.out(output), dim=1)
+        else:
+            words = torch.zeros([input.shape[0],1]) + self.sos_id
+            out = torch.zeros(input.shape[0], maxlen,self.embedding_size)
+            for i in range(max_len):
+                embs = self.embedding(words)
+                output, hidden = self.gru(embs, hidden)
+                scores = F.log_softmax(self.out(output), dim=1)
+                out[:,i,:] = scores
+                for j in range(input.shape[0]):
+                    words[j] = torch.argmax(scores[j])
+
         return output, hidden
 
