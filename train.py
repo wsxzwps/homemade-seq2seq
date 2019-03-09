eee00e3b2b360a82042bdf774427b85aca757fb8
wsxzwps <wsxzwps43@126.com>
2019-03-09 10:53:00 +0800

    some unknown bugs

--- train.py
+++ train.py
@@ -18,11 +18,11 @@ def train(input_tensor, target_tensor, target_lengths, encoder, decoder, criteri
         optimizer.zero_grad()
 
     loss = 0
-
+    maxlen = max(30, target_tensor.shape[1])
     # Get encoder hidden states and outputs
     output_e, hidden_e = encoder(input_tensor)
 
-    output_d, hidden_d = decoder(target_tensor[:,:-1], hidden_e, 90, teacher_forcing_ratio = teacher_forcing_ratio)
+    output_d, hidden_d = decoder(target_tensor[:,:-1], hidden_e, maxlen, teacher_forcing_ratio = teacher_forcing_ratio)
     # Define the loss function
     batch_size = output_d.shape[0]
     for i in range(batch_size):
