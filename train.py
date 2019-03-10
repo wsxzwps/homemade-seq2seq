40a01ec581f4a85c2b9b84b5a5296fe1837406c4
wsxzwps <wsxzwps43@126.com>
2019-03-11 04:52:08 +0800

    add rev_vocab

--- train.py
+++ train.py
@@ -111,7 +111,7 @@ def evaluate(loader, encoder, decoder):
         for i in range(output_d.shape[0]):
             sentence = []
             for j in range(output_d.shape[1]):
-                word = torch.topk(output_d[i,j,:], 1)[1].squeeze(1)
+                word = torch.topk(output_d[i,j,:], 0)[1]
                 sentence.append(word)
             rev_vocab(sentence)
 
